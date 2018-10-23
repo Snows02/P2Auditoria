acffdf59e76747193d5a8cade4dbc9db9d5007fb
Ivan Nieves Ardila <gykour.friki.gamer@gmail.com>
2018-10-23 13:25:27 -0500

    :v

--- auditoria/settings.py
+++ auditoria/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'auditoria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
